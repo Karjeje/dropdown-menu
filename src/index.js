12fb520374b25df8ac4ab0efaf4707fc6ded8316
Karjeje <193631637+Karjeje@users.noreply.github.com>
2025-10-30 17:05:29 +0100

    Write dropdown logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import "./style.css";
+import "./styles.css";
 
-console.log("Template");
+console.log("Init");
+
+export function setupDropdown(dropdownContainer) {
+  const button = document.querySelector(".menubtn");
+  const dropdown = document.querySelector(".dropdown");
+
+  button.addEventListener("click", () => {
+    dropdown.classList.toggle("invisible");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!dropdownContainer.contains(e.target)) {
+      dropdown.classList.add("invisible");
+    }
+  });
+}
+
+export function initDropdowns() {
+  document.querySelectorAll(".menucontainer").forEach(setupDropdown);
+}
